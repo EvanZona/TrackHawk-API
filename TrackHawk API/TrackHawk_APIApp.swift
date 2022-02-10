5808e0e6d025d3c5370c7ee222c5acf4fa04d463
Evan Zona <evanbzona@gmail.com>
2022-02-10 10:24:19 -0600

    Initial Commit

---
+++ TrackHawk API/TrackHawk_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackHawk_APIApp.swift
+//  TrackHawk API
+//
+//  Created by Student on 2/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TrackHawk_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
